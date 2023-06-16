f2f8f61a0105772e4a7bfddc7384242e31d60f9e
ALX2-Engineer <john7ouma@gmail.com>
2023-06-16 00:38:15 -0700

    i dont know whyits not working

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
+
 /**
- * print_dlistint - prints all elements of dlistint_t list.
- * @h: pointer to head of the list
- *
+ * print_dlistint- Prints all the elements of a dlistint_t list
+ * @h: Pointer to the first node
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int counter = 0;
-	if (h == NULL)
-		return (counter);
-	while (h->prev != NULL)
-		h = h->prev;
+	size_t l = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		counter++;
+		l++;
 	}
-
-	return (counter);
+	return (l);
 }
